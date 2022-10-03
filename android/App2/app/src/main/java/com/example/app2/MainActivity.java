80ae15abd2c7dc58938033db06caf27952620c65
Tiago Loureiro <metiagoloureiro@gmail.com>
2022-10-03 12:49:30 +0100

    App2_Git

--- android/App2/app/src/main/java/com/example/app2/MainActivity.java
+++ android/App2/app/src/main/java/com/example/app2/MainActivity.java
@@ -2,13 +2,20 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        public void Enviar(View view) {
+        Intent intent=new Intent(this,MainActivity2.class)
+            startActivity(intent)
+        }
+    }
